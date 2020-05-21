98bfffbd847b7da486df4e96df1e7039aa2fc9ea
Julia Paiva <43045380+juliavpaiva@users.noreply.github.com>
2020-05-21 15:58:19 -0300

    Updating tables

--- englix/models.py
+++ englix/models.py
@@ -3,28 +3,26 @@ import enum
 
 from app import db
 
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(25), nullable=False)
-
-    def __repr__(self):
-        return '<name %r, email %r, password %r, id %r>' % (self.name, self.email, self.password, self.id)
-
 class LevelType(enum.Enum):
     Intermadiate = 30
     Advanced = 60
     Fluent = 80
 
-class Level(db.Model):
+class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(25), nullable=False)
     level_type = db.Column(db.Enum(LevelType))
+
+    def __repr__(self):
+        return '<name %r, email %r, password %r, id %r>' % (self.name, self.email, self.password, self.id)
 
 class Lesson(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
-    
+    level_type = db.Column(db.Enum(LevelType))
+
     activities = db.relationship('Activity', backref='lesson', cascade="all,delete", lazy=True)
 
 class Activity(db.Model):
@@ -41,12 +39,13 @@ class QuizType(enum.Enum):
 class Quiz(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     quiz_type = db.Column(db.Enum(QuizType))
-    #conteúdo dependendo do  tipo de quiz: texto, video ou foto AR
+    content = db.Column(db.String(200), nullable=False)
     activity_id = db.Column(db.Integer, db.ForeignKey('activity.id'), nullable=False)
-    
     answers = db.relationship('Answer', backref='quiz', cascade="all,delete", lazy=True)
+
 
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
+    answer_type = db.Column(db.Boolean, default=False, nullable=False)
     quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
