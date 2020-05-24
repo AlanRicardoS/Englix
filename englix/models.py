d4f0d6d9d44cb47c9d3b81fcbb6b9dfbd991a494
Julia Paiva <43045380+juliavpaiva@users.noreply.github.com>
2020-05-24 16:08:47 -0300

    updating models

--- englix/models.py
+++ englix/models.py
@@ -26,31 +26,33 @@ class User(db.Model):
 class Lesson(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
     level_type = db.Column(db.Enum(LevelType))
 
     activities = db.relationship('Activity', backref='lesson', cascade="all,delete", lazy=True)
 
-class Activity(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    lesson_id = db.Column(db.Integer, db.ForeignKey('lesson.id'), nullable=False)
-    
-    quizes = db.relationship('Quiz', backref='activity', cascade="all,delete", lazy=True)
-
-class QuizType(enum.Enum):
+class ActivityType(enum.Enum):
     Video = "Video"
     AR = "AR"
     Text = "Text"
 
+class Activity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    lesson_id = db.Column(db.Integer, db.ForeignKey('lesson.id'), nullable=False)
+    content_file = db.Column(db.String(200), nullable=False)
+    activity_type = db.Column(db.Enum(ActivityType))
+    
+    quizes = db.relationship('Quiz', backref='activity', cascade="all,delete", lazy=True)
+
 class Quiz(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    quiz_type = db.Column(db.Enum(QuizType))
-    content = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.String(300), nullable=False)
     activity_id = db.Column(db.Integer, db.ForeignKey('activity.id'), nullable=False)
     answers = db.relationship('Answer', backref='quiz', cascade="all,delete", lazy=True)
 
 
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.String(300), nullable=False)
     answer_type = db.Column(db.Boolean, default=False, nullable=False)
     quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
